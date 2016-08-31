1e50f79301f8a6dec8893e3368a341819d4b1d5a
Nazmus Sazib <nazmus.s.sazib@aggiemail.usu.edu>
2016-08-31 15:19:08 -0600

    use watershed raster for finding subwatersehd ID that contains adjacent point which is faster

--- NHD_Rapid_Watershed_Delineation.py
+++ NHD_Rapid_Watershed_Delineation.py
@@ -35,9 +35,12 @@ def Point_Watershed_Function(longitude,latitude,snapping,maximum_snap_distance,P
        if (ID_Ocean_Stream!=None):
           sys.exit("POINT LOCATED IN SIDE THE OCEAN!")
 
-
+   gage_watershed_rasterfile = os.path.join(dir_main, gage_watershed_raster)
    #start_time = time.time()
-   fg=point_in_Polygon(dir_main,Main_watershed,point1)
+   ## extract ID from gage watershed raster saves significant amount of time, that is polygon searching takes long amount of time
+   ## however extract raster value from raster does not takes
+   fg=int(extract_value_from_raster_point(gage_watershed_rasterfile, longitude, latitude))
+   #fg=point_in_Polygon(dir_main,Main_watershed,point1)
    #print("find point in polygon %s seconds ---" % (time.time() - start_time2))
    ID=fg
    print(ID)
@@ -70,7 +73,11 @@ def Point_Watershed_Function(longitude,latitude,snapping,maximum_snap_distance,P
    #start_time = time.time()
    if(snaptostream==1):
      if((ID>0&(distance_stream<Distance_thresh))):
-        subprocess.call(MOVEOUTLETTOSTREAMS(MPH_dir,np,TauDEM_dir,Grid_dir,Grid_Name,Output_dir,Outlet_Point,Distance_thresh))
+        os.chdir(Output_dir)
+        #subprocess.call('AreaD8.exe')
+        subprocess.call(str(MOVEOUTLETTOSTREAMS(MPH_dir,np,TauDEM_dir,Grid_dir,Grid_Name,Output_dir,Outlet_Point,Distance_thresh)))
+        #os.chdir(Output_dir)
+
         os.chdir(Output_dir)
         outlet_moved_file=os.path.join(Output_dir,"New_Outlet.shp")
      else:
@@ -81,28 +88,50 @@ def Point_Watershed_Function(longitude,latitude,snapping,maximum_snap_distance,P
      os.chdir(Output_dir)
      subprocess.call(MOVEOUTLETTOSTREAMS(MPH_dir,np,TauDEM_dir,Grid_dir,Grid_Name,Output_dir,Outlet_Point,0)) ## not move
      outlet_moved_file=os.path.join(Output_dir,"New_Outlet.shp")
-
+   #os.chdir(Grid_dir)
    subprocess.call(GAUGE_WATERSHED(MPH_dir,np,TauDEM_dir,Grid_dir,Grid_Name,Output_dir,outlet_moved_file,New_Gage_watershed_Name))
    #new_watershed_raster=os.path.join(Output_dir,New_Gage_watershed_Name+".tif")
    cmd4='gdal_polygonize.py -8 local_subwatershed.tif -b 1 -f "ESRI Shapefile"  local_subwatershed.shp local_subwatershed GRIDCODE '
    os.system(cmd4)
-   cmd1='ogr2ogr local_subwatershed_dissolve.shp local_subwatershed.shp -dialect sqlite -sql'+ " "+ ' "SELECT GRIDCODE ,ST_Union(geometry) as geometry FROM '+" "+" 'local_subwatershed' " + " " +' GROUP BY GRIDCODE" '+ '  -nln results -overwrite'
-   os.system(cmd1)
+   cmd1='ogr2ogr local_subwatershed_dissolve.shp local_subwatershed.shp -dialect sqlite -sql'+ " "+ ' "SELECT GRIDCODE ,ST_Union(geometry) as geometry FROM '+" "+" 'local_subwatershed' " + " " +' GROUP BY GRIDCODE" ' + '  -nln results -overwrite'
+   print(cmd1)
+   subprocess.call(cmd1)
+   ##cmd3='ogr2ogr outfile.shp infile.shp - simplify 1' need to check later for testing
+   ##subprocess.call(cmd3)
    New_Gage_watershed_Dissolve=New_Gage_watershed_Name+ "_dissolve"
    myid=[]
-   gage_watershed_rasterfile=os.path.join(dir_main,gage_watershed_raster)
-   with open("upwacoor.txt", "rt") as f:
-       next(f)
+   subid=[]
+   start_time2 = time.time()
+   src_ds = gdal.Open(gage_watershed_rasterfile)
+   gt = src_ds.GetGeoTransform()
+   rb = src_ds.GetRasterBand(1)
+
+   num_lines = sum(1 for line in open('upwacoor.txt'))
+   if(num_lines)>1:
+    with open("upwacoor.txt", "rt") as f:
+       #next(f)
        for line in f:
            x = float(line.split(',')[0])
            y = float(line.split(',')[1])
-           myid.append(extract_value_from_raster_point(gage_watershed_rasterfile,x,y))
-   subid=list(set(myid))
-   print(subid)
+           mx = x
+           my = y
+           # ,my=geom.GetX(), geom.GetY()  #coord in map units
+           ## using this approach is the fastest than others such as using gdallocation info or extract raster
+           px = int((mx - gt[0]) / gt[1])  # x pixel
+           py = int((my - gt[3]) / gt[5])  # y pixel
+           Pixel_Data = rb.ReadAsArray(px, py, 1, 1)  # Assumes 16 bit int aka 'short'
+           Pixel_Val = Pixel_Data[0, 0]  # use the 'short' format code (2 bytes) not int (4 bytes)
+           #cmd='gdallocationinfo -valonly -b 1'+' '+gage_watershed_rasterfile +' '+str(x) +' '+ str(y)
+          # result =subprocess.check_output(('gdallocationinfo'+' '+gage_watershed_rasterfile+' '+' -geoloc '+str(x)+' '+str(y)+ ' -valonly'))
+           # myid.append(extract_value_from_raster_point(gage_watershed_rasterfile,x,y))
 
+           myid.append(int( Pixel_Val ))
+    subid=list(set(myid))
+   # print(subid)
+    print("identify upstream watershed %s seconds ---" % (time.time() - start_time2))
 
    #start_time = time.time()
-   if(ID>0):
+   if((ID>0)&(num_lines>1)):
     compli_watershed_ID= [i for i in subid if i >0]
     len_comp=len(subid)
    else:
@@ -129,7 +158,8 @@ def Point_Watershed_Function(longitude,latitude,snapping,maximum_snap_distance,P
           cmd2='ogr2ogr -update -append'+ " "+sub_water_file[0]+ " " + sub_water_file[x]
           print(cmd2)
           os.system(cmd2)
-      cmd='ogr2ogr New_Point_Watershed.shp local_subwatershed_dissolve.shp -dialect sqlite -sql'+ " "+ ' "SELECT GRIDCODE ,ST_Union(geometry) as geometry FROM '+" "+" 'local_subwatershed_dissolve' " + " " +' GROUP BY GRIDCODE" '
+      ## adding st_buffer remove the gaps between polygons
+      cmd='ogr2ogr New_Point_Watershed.shp local_subwatershed_dissolve.shp -dialect sqlite -sql'+ " "+ ' "SELECT GRIDCODE ,ST_Union(ST_buffer(geometry, 30)) as geometry FROM '+" "+" 'local_subwatershed_dissolve' " + " " +' GROUP BY GRIDCODE" '
       os.system(cmd)
    else:
       flag="Up stream edge was Not reached"
@@ -167,9 +197,6 @@ def Point_Watershed_Function(longitude,latitude,snapping,maximum_snap_distance,P
 
 
 
-if __name__ == '__main__':
-    # Map command line arguments to function arguments.
-    Point_Watershed_Function(*sys.argv[1:])
 
 
 
